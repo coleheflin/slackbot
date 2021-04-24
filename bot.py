a3b9032e16a197844fb01d297a6cf3a720fa28b3
Cole Heflin <cvheflin@gmail.com>
2021-04-24 15:47:24 -0700

    sending welcome message

--- bot.py
+++ bot.py
@@ -20,6 +20,58 @@ SLACK_CLIENT = slack.WebClient(token=SLACK_TOKEN)
 BOT_ID = SLACK_CLIENT.api_call("auth.test")["user_id"]
 # SLACK_CLIENT.chat_postMessage(channel='#tutorial', text="Hello")
 message_counts = {}
+welcome_messages = {}
+
+
+class WelcomeMessage:
+    START_TEXT = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": (
+                "Welcome to this awesome channel! \n\n"
+                "*Get started by completing the tasks!*"
+            ),
+        },
+    }
+
+    DIVIDER = {"type": "divider"}
+
+    def __init__(self, channel, user):
+        self.channel = channel
+        self.user = user
+        self.icon_emoji = ":robot_face:"
+        self.timestamp = ""
+        self.completed = False
+
+    def get_message(self):
+        return {
+            "ts": self.timestamp,
+            "channel": self.channel,
+            "username": "Welcome Robot!",
+            "icon_emoji": self.icon_emoji,
+            "blocks": [self.START_TEXT, self.DIVIDER, self._get_reaction_task()],
+        }
+
+    def _get_reaction_task(self):
+        checkmark = ":white_check_mark:"
+        if not self.completed:
+            checkmark = ":white_large_square:"
+
+        text = f"{checkmark} *React to this message!*"
+
+        return {"type": "section", "text": {"type": "mrkdwn", "text": text}}
+
+
+def send_welcome_message(channel, user):
+    welcome = WelcomeMessage(channel, user)
+    message = welcome.get_message()
+    response = SLACK_CLIENT.chat_postMessage(**message)
+    welcome.timestamp = response["ts"]
+
+    if channel not in welcome_messages:
+        welcome_messages[channel] = {}
+    welcome_messages[channel][user] = welcome
 
 
 @SLACK_EVENT_ADAPTER.on("message")
@@ -29,11 +81,14 @@ def message(payload):
     user_id = event.get("user")
     text = event.get("text")
     # Only process messages from the user, IGNORE THE BOT MESSAGES
-    if BOT_ID != user_id:
+    if user_id != None and BOT_ID != user_id:
         if user_id in message_counts:
             message_counts[user_id] += 1
         else:
             message_counts[user_id] = 1
+
+        if text.lower() == "start":
+            send_welcome_message(f"@{user_id}", user_id)
 
 
 # Slash Commands
